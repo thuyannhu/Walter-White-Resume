90c4dade23c341e8f2bbb9a976c0cfc403c8277e
thuyannhu <nhu.thuy-an@hotmail.fr>
2023-03-17 12:24:35 +0100

    ajout instructions navbar ajout fonction js pour index

--- asset/js/index.js
+++ asset/js/index.js
@@ -16,3 +16,12 @@ function menuBurger() {
       theme.setAttribute('href', 'asset/styles/light_style.css');
     }
   }  
+
+  function toggleThemeIndex() {
+    var theme = document.getElementsByTagName('link')[0];
+    if(theme.getAttribute('href') == 'asset/styles/light_index.css') {
+      theme.setAttribute('href', 'asset/styles/dark_index.css');
+    } else {
+      theme.setAttribute('href', 'asset/styles/light_index.css');
+    }
+  }  
